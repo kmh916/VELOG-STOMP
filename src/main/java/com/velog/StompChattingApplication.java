645c89ff3fddc1d904174dc3f809653805e6b0f1
김민혁 <94831670+kmh916@users.noreply.github.com>
2022-06-21 14:14:50 +0900

    initail commit

---
+++ src/main/java/com/velog/StompChattingApplication.java
@@ -0,0 +1,11 @@
+package com.velog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StompChattingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StompChattingApplication.class,args);
+    }
+}
